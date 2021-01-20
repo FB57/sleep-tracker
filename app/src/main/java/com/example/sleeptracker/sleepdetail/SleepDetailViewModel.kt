8726c17a5cd72d1b0d712111436de929b51aa155
FB57 <fenildbardoliya@gmail.com>
2021-01-20 23:13:40 +0530

    sleep-tracker-complete

---
+++ app/src/main/java/com/example/sleeptracker/sleepdetail/SleepDetailViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.sleeptracker.sleepdetail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.sleeptracker.database.SleepDatabaseDao
+import com.example.sleeptracker.database.SleepNight
+import kotlinx.coroutines.Job
+
+class SleepDetailViewModel(
+    private val sleepNightKey: Long = 0L,
+    dataSource: SleepDatabaseDao) : ViewModel() {
+
+    val database = dataSource
+
+    private val viewModelJob = Job()
+
+    private val night = MediatorLiveData<SleepNight>()
+
+    fun getNight() = night
+
+    init {
+        night.addSource(database.getNightWithId(sleepNightKey), night::setValue)
+    }
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() =_navigateToSleepTracker
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onClose() {
+        _navigateToSleepTracker.value = true
+    }
+}
